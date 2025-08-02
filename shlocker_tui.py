9c2cc049c7793b8d900dcbeab1d1c5a68793885b
Wook <mr.wook@gmail.com>
2025-08-02 14:39:22 -0700

    Added rich colors, rm, persist, restore, forget, help;

--- shlocker_tui.py
+++ shlocker_tui.py
@@ -6,14 +6,17 @@ if True:
     from   functools import partial
     import json
     import os
-    from   logger import logging
+    import logging # from   logger import logger
     from   pathlib import Path
     import pdb
     import platformdirs
     from   pprint import pprint, pformat
     import re
+    from   rich import print as rprint
+    from   rich.console import Console
     import subprocess
     import sys
+    import time
 
     # sys.path.insert(1, ".")
     from tui3 import Tui3
@@ -27,11 +30,12 @@ class ServerClient:
 
 if __name__ == "__main__":
     class App:
-        LOGFILE = "/var/log/shlocker"
+        LOGFILE = "/var/log/shlocker/shlocker.log"
         REXHEX = re.compile(r'[0-9a-fA-F]+')
         def __init__(self):
+            self._console = Console()
             self.data_home = platformdirs.user_data_dir("shlocker", "mr.wook@gmail.com")
-            self._width = self._get_width()
+            self._width = self._get_width() - 1
             self._logger_setup()
             self._ui = Tui3(prompt="shlocker> ")
             self._set_dispatch()
@@ -40,9 +44,41 @@ if __name__ == "__main__":
             self._persistence = self.load()
             return
 
+        def clean(self, ui, *args):
+            # 'all' means all non-running containers;
+            # 'junk' means all non-running containers with no name;
+            junk = all_ = False
+            if not args:
+                junk = True
+                all_ = False
+            elif 'all' in args:
+                all_ = True
+            elif 'junk' in args:
+                junk = True
+            images = self._docker.images.list(all=True)
+            untagged = [img for img in images if not img.tags]
+            untagged_ids = [img.short_id[7:] for img in untagged]
+            running_ids = [xc.short_id for xc in self._docker.containers.list(all=True) if xc.status == 'running']
+            stopped = [xc for xc in self._docker.containers.list(all=True) if xc.status == 'stopped']
+            non_running_images = [xi for xi in images if xi.short_id[7:] not in running_ids] # Remember RepoTags;
+            target_images = non_running_images
+            removals = self._docker.containers.prune()
+            pprint(removals)
+            return True
+            for xt in target_images:
+                xt_attrs = xt.attrs
+                rt = xt_attrs['RepoTags'] if xt_attrs['RepoTags'] else ""
+                # breakpoint()
+                name = xt.tags[0] if xt.tags else "<none>"
+                if name != "<none>":
+                    continue
+                print(f"{name} {xt.short_id[7:]} {rt}")
+                xt.remove()
+            return True
+
         def _columnize(self, list_):
             max_col_width = max([len(item) for item in list_]) + 1
-            n_cols = (self._width // max_col_width) -1
+            n_cols = (self._width // max_col_width) - 1
             ostr = ""
             n_items = 0
             for item in list_:
@@ -62,6 +98,36 @@ if __name__ == "__main__":
             print(" ".join([str(arg) for arg in args]))
             return True
 
+        def forget(self, ui, *args):
+            containers = self._persistence
+            if not containers:
+                print(f"No persistent containers")
+                return False
+            all_containers = self._all_containers
+            if args == ( None, ):   # No args artifact, forget all; -- if not ui.args?
+                self._persistence = [ ]
+                self.save()
+                self._info("All containers unpersisted")
+                return True
+            else:
+                forgotten = 0
+                for c_spec in args:
+                    cont = self._match(c_spec)
+                    if not cont:
+                        print(f"No container matched spec {c_spec}")
+                        continue
+                    c_id = cont.id
+                    if not c_id in self._persistence:
+                        continue
+                    print(f"Forgetting {cont.id} ({cont.id})")
+                    del self._persistence[c_id]
+                    forgotten += 1
+            # pprint(self._persistence)
+            if forgotten:
+                self._info(f"{forgotten} containers forgotten")
+                self.save()
+            return True
+
         def _get_width(self):
             subp = subprocess.Popen(['tput', 'cols'], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             so, se = subp.communicate()
@@ -72,6 +138,24 @@ if __name__ == "__main__":
             else:
                 width = 100
             return width
+
+        def _gray(self, txt, rc=True):
+            self._print(f"[gray]{txt}")
+            return rc
+
+        def help(self, ui, *args):
+            cmdnames = list(ui.cmdnames)
+            specials = [cmd for cmd in cmdnames if cmd.startswith('%')]
+            cmds = [cmd for cmd in cmdnames if not cmd.startswith('%')]
+            cmds.sort()
+            specials.sort()
+            print(self._columnize(specials))
+            print(self._columnize(cmds))
+            return True
+
+        def _info(self, txt, rc=True):
+            self._print(f"[blue][{txt}]")
+            return rc
 
         def info(self, ui, *args):
             which = args[0]
@@ -96,6 +180,8 @@ if __name__ == "__main__":
             return ostr
  
         def _is_hex(self, s: str):
+            if s is None:
+                return False
             return App.REXHEX.match(s)
 
         def load(self, *args) -> dict:
@@ -114,7 +200,7 @@ if __name__ == "__main__":
             return json.loads(ibuf)
 
         def _logger_setup(self):
-            log_file = "/var/www/shlocker/shlocker.log"
+            log_file = App.LOGFILE
             os.makedirs(os.path.dirname(log_file), exist_ok=True)
             logging.basicConfig(filename=log_file, filemode='a',  # append mode
                                 format='%(asctime)s [%(levelname)s] %(message)s',
@@ -123,20 +209,34 @@ if __name__ == "__main__":
 
         def ls(self, ui, *args):
             self.reload()
+            colors = dict(running='green', exited='red')
             which = [ 'running' ]
             if '--all' in args:
                 which = list(self._by_state.keys())
-            visible = [ 'name', 'version' ]
             for state in which:
-                print(state.upper())
-                for c in self._by_state[state]:
-                    view = self._view(c)
-                    print(view)
+                if not self._by_state[state]:
+                    continue
+                self._print(state.upper(), style="bold")
+                color = "default"
+                if state in colors:
+                    color = colors[state]
+                for xc in self._by_state[state]:
+                    view = self._view(xc)
+                    if xc.id in self._persistence:
+                        self._print(f"[{color}] * {view}")
+                    else:
+                        self._print(f"[{color}]{view}")
             return True
 
-        def _match(self, id_or_name, containers = None):
-            if containers is None:
-                containers = self._by_state['running']
+        def _match(self, id_or_name, which_containers = 'running'):
+            match which_containers:
+                case 'all':
+                    containers = self._all_containers
+                case None:
+                    containers = self._all_containers
+                case _:
+                    containers = self._by_state[which_containers]
+
             if self._is_hex(id_or_name):
                 c_matches = [c for c in containers if c.id.endswith(id_or_name)]
                 if not c_matches:
@@ -146,6 +246,7 @@ if __name__ == "__main__":
                     print(f"Ambiguous {id_or_name}")
                     return False
                 return c_matches[0]
+
             c_matches =  [c for c in containers if c.name == id_or_name]
             if not c_matches:
                 return False
@@ -157,12 +258,18 @@ if __name__ == "__main__":
                 return False
             return c_matches[0]
 
+        def pdb(self, *args):
+            breakpoint()
+            return True
+
         def persist(self, ui, *args):
             containers = self._by_state['running']
             if not containers:
                 print(f"No running containers, nothing to persist")
+                self._review_persistent()
                 return False
             if args == ( None, ):   # No args artifact;
+                # breakpoint()
                 for c in containers:
                     if c.id not in self._persistence:
                         self._persistence[c.id] = True
@@ -177,7 +284,11 @@ if __name__ == "__main__":
                         continue
                     self._persistence[c.id] = True
             pprint(self._persistence)
+            self.save()
             return True
+
+        def _print(self, *args, **kwa):
+            self._console.print(*args, **kwa)
 
         def pstat(self, ui, *args):
             if args == ( None, ):
@@ -219,12 +330,64 @@ if __name__ == "__main__":
         def quitter(self, ui, *args):
             sys.exit(0)
 
+        def _red(self, txt, rc=True):
+            self._print(f"[red]{txt}")
+            return rc
+
         def reload(self, *args):
+            "Get status for all containers, and group by state;"
             self._all_containers = all_ = self._docker.containers.list(all=True)
             states = list(set([c.status for c in all_]))
-            self._by_state = dict()
+            self._by_state = dict(running=[], exited=[])  # Sleazy hack;
             for state in states:
                 self._by_state[state] = [c for c in all_ if c.status == state]
+            return True
+
+        def restart(self, ui, *args):
+            return self.start_stop("restart", *ui.args)
+
+        def restore(self, *args, **kwa):
+            if not self._persistence:
+                self._red("Nothing persistent to restore")
+            _all_containers = self._all_containers
+            _all_container_ids = [c.id for c in _all_containers]
+            restored = 0
+            for pc_id in self._persistence:
+                if pc_id not in _all_container_ids:
+                    self._red("No container {pc_id} available to restore")
+                    continue
+                cont = [rcont for rcont in _all_containers if pc_id == rcont.id][0]
+                cont.start()
+                restored += 1
+                self._print(f"[blue]{self._view(cont)}")
+            self._info(f"{restored} containers restarted")
+            return restored
+
+        def _review_persistent(self):
+            _all_containers = self._all_containers
+            _all_container_ids = [c.id for c in _all_containers]
+            for pc_id in self._persistence:
+                if pc_id not in _all_container_ids:
+                    self._console.print("[gray]{pc_id} not found")
+                    continue
+                c = [c for c in _all_containers if c.id == pc_id][0]
+                print(self._view(c))
+            return
+
+        def rm(self, ui, *args):
+            if not ui.args:
+                print(f"No container specified for removal")
+                return False
+            self.reload()
+            c = self._match(ui.args[0], 'all')
+            if not c:
+                print(f"No such container")
+                return False
+            if c.status == 'running':
+                print(f"Won't stop running container {c.name}")
+                return False
+            print(f"Removing {c.name} ({c.id})")
+            rc = c.remove()
             return True
 
         def run(self):
@@ -249,17 +412,24 @@ if __name__ == "__main__":
 
         def _set_dispatch(self):
             ui = self._ui
+            ui.add("clean", self.clean)
+            ui.add("forget", self.forget) # unpersist;
+            ui.add("help", self.help)
             ui.add("info", self.info)
             ui.add("%info", self._container_info)
             ui.add("ls", self.ls)
+            ui.add("pdb", self.pdb)
             ui.add("persist", self.persist)
             ui.add("%ps", self.pstat)
             ui.add("qq", self.quitter)
             ui.add("quit", self.quitter)
-            ui.add("save", self.save)
             # ui.add("server", self.set_server)
             ui.add("reload", self.reload)
-            ui.add("restart", partial(self.start_stop, "restart", *ui.args))
+            ui.add("remember", self.persist)
+            ui.add("restart", self.restart)
+            ui.add("restore", self.restore)
+            ui.add("rm", self.rm)
+            ui.add("save", self.save)
             ui.add("start", self.start)
             ui.add("stop", self.stop)
             ui.add("zz", self.quitter)
@@ -274,12 +444,13 @@ if __name__ == "__main__":
             c.stop()
 
         def start_stop(self, which, *args):
-            if args == ( None, ):
+            ui = self._ui
+            if not ui.args:
                 print(f"{which} needs a container identifier")
                 return False
-            c = self._match(args[0])
-            print(str(c))
-            # Convert this to match which;
+            c = self._match(ui.args[0], 'all') # start and restart need to match against stopped containers;
+            print(self._view(c))
+            # NONE OF THIS WORKS RIGHT!
             match which:
                 case 'stop':
                     if not self.running_p(c):
@@ -297,14 +468,14 @@ if __name__ == "__main__":
                     time.sleep(0.5)
                     print(self._view(c))
                 case 'restart':
-                    if not self.running_p(c):
-                        self._start__log(c)
-                    else:
+                    if self.running_p(c):
+                        print(f"([stopping {c.name}])")
                         self._stop__log(c)
                         time.sleep(2)
-                        self._start__log(c)
-                        time.sleep(2)
-                        print(self._view(c))
+                    print(f"[starting {c.name}]")
+                    self._start__log(c)
+                    time.sleep(2)
+                    print(self._view(c))
             self.load()
             return True
 
@@ -315,11 +486,21 @@ if __name__ == "__main__":
             return self.start_stop("stop", *args)
 
         def _view(self, c):
+            visible = [ 'name', 'version' ]
             labels = [f"{k}:{c.labels[k]}" for k in visible if k in c.labels]
             # print(f"{c.name:<16s} {str(c.id)[-12:]} {c.status}  {' '.join(labels)} {c.ports} {c.top}")
             ports = self._port_mappings(c)
             mounts = self._mount_bind_mappings(c)
             return f"{c.name:<16s} ({str(c.id)[-12:]}) {ports} {mounts}"
+
+        @property
+        def all_containers(self):
+            return self._docker.containers.list(all=True)
+
+        @property
+        def all_images(self):
+            return self._docker.images.list(all=True)
+
 
     app = App()
     app.run()
